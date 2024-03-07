747ed58da2906e7f17bacd76ff3ac660457c801a
mrjury76 <mrjury76@gmail.com>
2024-03-06 16:04:38 -0800

    adding second activity view

--- Final Project Folder/app/src/main/java/com/example/finalprojectcontribution/Account.java
+++ Final Project Folder/app/src/main/java/com/example/finalprojectcontribution/Account.java
@@ -1,9 +1,7 @@
 package com.example.finalprojectcontribution;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Account extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.account);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.accountxml), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button account = findViewById(R.id.accountButton);
-
-        account.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent accountView = new Intent(getApplicationContext(), Account.class);
-            }
         });
     }
 }
